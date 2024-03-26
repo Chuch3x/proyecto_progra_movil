5c48347308b5d4790460ba194b2c9e8dd729a556
Chuch3x <c.augusto_mendez@hotmail.com>
2024-03-26 09:19:52 -0400

    corrected errors

--- lib/register/bloc_builder.dart
+++ lib/register/bloc_builder.dart
@@ -41,7 +41,7 @@ class _RegisterState extends State<RegisterScreen> {
     return TextFormField(
       controller: _userController,
       decoration: InputDecoration(
-        icon: Icon(Icons.person),
+        icon: const Icon(Icons.person),
         labelText: textHint,
       ),
       validator: (String? value) {
@@ -54,7 +54,7 @@ class _RegisterState extends State<RegisterScreen> {
     return TextFormField(
       controller: _emailController,
       decoration: InputDecoration(
-        icon: Icon(Icons.mail),
+        icon: const Icon(Icons.mail),
         labelText: textHint,
       ),
       validator: (String? value) {
@@ -69,7 +69,7 @@ class _RegisterState extends State<RegisterScreen> {
     return TextFormField(
       controller: type == 0 ? _passwordController : _password2Controller,
       decoration: InputDecoration(
-        icon: Icon(Icons.lock),
+        icon: const Icon(Icons.lock),
         labelText: textHint,
       ),
       obscureText: true,
